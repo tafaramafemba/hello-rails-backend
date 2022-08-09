37791defa994e2e6acd9f1a9d688a9061c89d80b
tafaramafemba <tafara.maf@gmail.com>
2022-08-09 18:39:44 +0200

    Amend api controller

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,8 +2,15 @@ module Api
   module V1
     class GreetingsController < ApplicationController
       def index
-        greeting = Greeting.order(Arel.sql('RANDOM()')).first
-        render json: { greeting: greeting.greeting }
+        greetings = Greeting.all
+
+        render json: GreetingSerializer.new(greetings).serialized_json
+      end
+
+      def show
+        greeting = Greeting.find(params[:id])
+
+        render json: GreetingSerializer.new(greeting).serialized_json
       end
     end
   end
